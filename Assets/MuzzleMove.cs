b56d5d0a4782b9aa1fa13cd3345fc98dcf58cde7
SasakiHayato0605 <zuomusundou685@gmail.com>
2021-08-12 21:52:10 +0900

    Playerの挙動修正

---
+++ Assets/MuzzleMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuzzleMove : MonoBehaviour
+{
+    [SerializeField] float m_distans = 0;
+    void Update()
+    {
+        Transform playerPos = transform.root;
+
+        float h = Input.GetAxisRaw("R_Horizontal");
+        float v = Input.GetAxisRaw("R_Vertical") * -1;
+        
+        if (h != 0 || v != 0)
+        {
+            Dir(h, v, playerPos);
+        }
+        else
+        {
+            transform.position = playerPos.position;
+        }
+    }
+
+    private void Dir(float h, float v, Transform playerPos)
+    {
+        float x = playerPos.position.x + h;
+        float y = playerPos.position.y + v;
+
+        transform.position = new Vector2(x, y) * m_distans;
+    }
+}
